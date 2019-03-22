23eda85a9d3b2e899bc709052831c75c2db4a5e6
HrachA <hrach.ayunc@gmail.com>
2019-03-22 22:31:43 +0400

    First commit

---
+++ Functions.cpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<vector>
+using namespace std;
+using namespace cv;
+
+
+
+
---
+++ Main.cpp
@@ -0,0 +1,41 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<vector>
+#include"Functions.cpp"
+
+using namespace std;
+using namespace cv;
+
+
+//returns the numbers of the barcode
+vector<int> GetInfo(Mat& img)
+{
+
+	return vector<int>();
+}
+
+//shows product information from the barcode numbers
+void showInfo(vector<int> vec)
+{
+
+}
+
+//recovers barcode from the barcode numbers
+Mat recoverBarcode(vector<int> vec)
+{
+
+	return Mat();
+}
+
+int main()
+{
+	Mat img = imread("example2.png");
+	namedWindow("Window", WINDOW_AUTOSIZE & WINDOW_NORMAL);
+
+	
+
+	imshow("Window", img);
+	//	imwrite("grabCut.png", img);
+	waitKey(0);
+	return 0;
+}
